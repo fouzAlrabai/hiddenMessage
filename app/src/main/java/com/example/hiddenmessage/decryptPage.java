d078785db9b475f4f38482998f1c025bbe757fc6
HussaSq <hessahsaud10@gmail.com>
2020-03-01 22:16:15 +0300

    backEnd

--- app/src/main/java/com/example/hiddenmessage/decryptPage.java
+++ app/src/main/java/com/example/hiddenmessage/decryptPage.java
@@ -4,17 +4,188 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class decryptPage extends AppCompatActivity {
+    EditText mMess,mKey;
+    Button decrypt;
+    TextView plain1;
+    String userText,key;
+    int colNum,mod,rowNum;
+    int plainText[][];
+    int cipherText[][];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decrypt_page);
+        mMess = findViewById(R.id.editText3);
+        mKey = findViewById(R.id.editText2);
+        decrypt = findViewById(R.id.button);
+        plain1 = findViewById(R.id.editText);
+
+        decrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userText =mMess.getText().toString();
+                key=mKey.getText().toString();
+
+                //check fields
+
+                if (TextUtils.isEmpty(userText)) {
+                    mMess.setError("Please Enter Message, It is Required");
+                    return;
+                }
+                //check fields
+
+                if (TextUtils.isEmpty(key)) {
+                    mKey.setError("Please Enter Key, It is Required");
+                    return;
+                }
+
+                userText =mMess.getText().toString();
+                key=mKey.getText().toString();
+
+                dycrypt(userText,key);
+            }
+        });
+
+
     }
 
     public void OpenMainPage(View view) {
         startActivity(new Intent(decryptPage.this,MainActivity.class));
+    }
+
+    public void dycrypt(String userText,String key){
+        //first is row then col
+        colNum=key.length();
+        mod =userText.length()%colNum;
+        rowNum=(userText.length()/colNum)+2+(mod);
+
+        System.out.println(rowNum);
+        char[][]plain=new char[rowNum+2][colNum];
+        //to put key at first row
+        for(int i =0;i<key.length();i++) {
+            plain[0][i]=key.charAt(i);
+        }
+        //to sort key
+        char[] ke = new char[key.length()];
+        char[] keSort=new char[key.length()];
+        for(int i =0;i<key.length();i++) {
+            ke[i]=plain[0][i];
+            keSort[i]=plain[0][i];
+        }
+
+        Arrays.sort(keSort);
+        //insert index
+        for(int i =0;i<colNum;i++) {
+            for (int j=0;j<colNum;j++ ) {
+                if( keSort[j]== ke[i] ){
+                    String n = j+1+"";
+                    plain[1][i]=n.charAt(0);
+                }
+            }
+
+        }
+        int index =0;
+        for(int i =2;i<rowNum;i++) {
+            for (int j=0;j<key.length();j++ ) {
+                if(index < userText.length()) {
+                  //  System.out.println(rowNum+"row");
+                    plain[i][j]=userText.charAt(index);
+                   // System.out.println("infd"+index);
+                  //  System.out.println(userText.charAt(index)+"c");
+                  //  System.out.println(""+plain[i][j]);
+                    index++;
+
+                }
+                //else if(index == userText.length()) {
+                // plain[i][j]='-';
+                // }
+
+            }
+        }
+        //to encrupt
+        String plainT="";
+
+
+
+        //to decrypt
+        // String key1="spyman";
+
+        char[][]encrypted =new char[rowNum][colNum];
+        for(int i =0;i<key.length();i++) {
+            encrypted[0][i]=key.charAt(i);
+        }
+
+        for(int i =0;i<key.length();i++) {
+            for (int j=0;j<key.length();j++ ) {
+                if( keSort[j]== ke[i] ){
+                    String n = j+1+"";
+                    encrypted[1][i]=n.charAt(0);
+                }
+            }
+
+        }
+
+        // now the real decrypt
+       // String text=ciphir;
+
+        index =0;
+        int f=49;
+        int loopsize;
+        int mod1=userText.length()%colNum;
+       // System.out.println("Mod*************************88"+mod1);
+       // System.out.println("Mod*****ROW***************88"+rowNum);
+        if(mod1 == 0)
+            loopsize =rowNum;
+        else
+            loopsize=rowNum-1;
+        //	for(int i =2;i<rowNum;i++) {
+        //System.out.println("row "+i);
+        for (int j=0;j<key.length();j++ ) {
+            // System.out.println("col "+plain[i][j]);
+            if(index < userText.length()) {
+                for(int k=0;k<key.length();k++) {
+                    int x =encrypted[1][k];
+                   // System.out.println("x = "+x);
+                  //  System.out.println("real = "+encrypted[1][k]);
+                    if( x == f) {
+                       // System.out.println("inside if for de ");
+                        for(int q =2;q<rowNum;q++) {
+                            if(index<userText.length())
+                                encrypted[q][k]=userText.charAt(index);
+                            index++;
+                        }
+                        //text.replace(text.charAt(index), ' ');
+                        //if(index+1<text.length() && i+1<rowNum) {
+                        //encrypted[i+1][k]=text.charAt(index+1);
+                        //text.replace(text.charAt(index+1), ' ');
+                        //}
+                        // index++;
+                        f++;
+                    }
+                }
+            }
+        }
+        // }
+        //******************************
+        for (int i = 2; i < encrypted.length; i++) {
+            for (int j = 0; (encrypted[i] != null && j < encrypted[i].length); j++)
+                plainT =plainT+encrypted[i][j] +"";
+
+
+        }
+       // plain.setText();
+        plain1.setText(plainT);
+
     }
 }
--- app/src/main/java/com/example/hiddenmessage/encryptPage.java
+++ app/src/main/java/com/example/hiddenmessage/encryptPage.java
@@ -4,17 +4,141 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Arrays;
 
 public class encryptPage extends AppCompatActivity {
 
+    EditText mMess,mKey;
+    Button encrypt;
+    TextView cipher ;
+     String userText,key;
+     int colNum,mod,rowNum;
+     int plainText[][];
+     int cipherText[][];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encrypt_page);
+        mMess = findViewById(R.id.editText3);
+        mKey = findViewById(R.id.editText2);
+        encrypt = findViewById(R.id.button);
+        cipher = findViewById(R.id.editText);
+
+
+
+        encrypt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userText =mMess.getText().toString();
+                key=mKey.getText().toString();
+
+                //check fields
+
+                if (TextUtils.isEmpty(userText)) {
+                    mMess.setError("Please Enter Message, It is Required");
+                    return;
+                }
+                //check fields
+
+                if (TextUtils.isEmpty(key)) {
+                    mKey.setError("Please Enter Key, It is Required");
+                    return;
+                }
+
+                userText =mMess.getText().toString();
+                key=mKey.getText().toString();
+
+                encrypt(userText,key);
+            }
+        });
+
     }
 
     public void OpenMainPage(View view) {
         startActivity(new Intent(encryptPage.this,MainActivity.class));
+    }
+
+    public void encrypt(String userText,String key){
+        colNum=key.length();
+        mod =userText.length()%colNum;
+        rowNum=(userText.length()/colNum)+2+(mod);
+
+       // System.out.println(rowNum);
+        char[][]plain=new char[rowNum+2][colNum];
+        //to put key at first row
+        for(int i =0;i<key.length();i++) {
+            plain[0][i]=key.charAt(i);
+        }
+        //to sort key
+        char[] ke = new char[key.length()];
+        char[] keSort=new char[key.length()];
+        for(int i =0;i<key.length();i++) {
+            ke[i]=plain[0][i];
+            keSort[i]=plain[0][i];
+        }
+
+        Arrays.sort(keSort);
+        //insert index
+        for(int i =0;i<colNum;i++) {
+            for (int j=0;j<colNum;j++ ) {
+                if( keSort[j]== ke[i] ){
+                    String n = j+1+"";
+                    plain[1][i]=n.charAt(0);
+                }
+            }
+
+        }
+        int index =0;
+        for(int i =2;i<rowNum;i++) {
+            for (int j=0;j<key.length();j++ ) {
+                if(index < userText.length()) {
+                   // System.out.println(rowNum+"row");
+                    plain[i][j]=userText.charAt(index);
+                    //System.out.println("infd"+index);
+                    //System.out.println(userText.charAt(index)+"c");
+                    //System.out.println(""+plain[i][j]);
+                    index++;
+
+                }
+                //else if(index == userText.length()) {
+                // plain[i][j]='-';
+                // }
+
+            }
+        }
+        //to encrypt
+        String ciphir="";
+        index =0;
+        int h=49;
+        for(int i =2;i<rowNum;i++) {
+            System.out.println("row "+i);
+            for (int j=0;j<key.length();j++ ) {
+                System.out.println("col "+plain[i][j]);
+                if(index < userText.length()) {
+                    for(int k=0;k<key.length();k++) {
+                        int x =plain[1][k];
+                        System.out.println("x = "+x);
+                        System.out.println("real = "+plain[1][k]);
+                        if( x == h) {
+                            System.out.println("inside if ");
+                            for(int w=2;w<rowNum;w++)
+                                ciphir=ciphir+plain[w][k];
+
+                            index++;
+                            h++;
+                        }
+                    }
+                }
+            }
+        }
+
+        cipher.setText(ciphir);
+
     }
 }
